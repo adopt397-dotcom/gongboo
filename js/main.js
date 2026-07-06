2420c2864f7d623a622e3f7a5e9aaf814e10b69b
adopt397-dotcom <adopt397@gmail.com>
2026-07-06 12:56:50 -0700

    Update API_URL to new endpoint

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ var LANG = {
 // ============================================================
 // 0200 - API URL
 // ============================================================
-var API_URL = "https://script.google.com/macros/s/AKfycbwYnCi7myER0R4djAV7CLW9Y1aTa-mjFSk_y_8vCD_p8vN78Sr5JeUB0WEqJR0_OTuG/exec";
+var API_URL = "https://script.google.com/macros/s/AKfycbwYnCi7myER0R4djAV7CLW9Y1aTa-mjFSk_y_8vcD_p8vN78Sr5JeUB0WEqJR0_OTuG/exec";
 var ORIGINAL_API_URL = API_URL;
 var MEMBER_API_URL = API_URL;
 
